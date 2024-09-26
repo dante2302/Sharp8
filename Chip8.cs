1335accd44c1f83c5cea72d8b0059d663b606965
Dante <guchoo923@gmail.com>
2024-09-26 14:04:33 +0300

    Add register declarations

--- Chip8.cs
+++ Chip8.cs
@@ -2,32 +2,42 @@ namespace Sharp8;
 
 public class Chip8
 {
-    private readonly ushort RomStart = 0x200; 
-    private byte[] Memory = new byte[4096];
-    private void SetupFonts()
+    // Memory
+    private ushort RomStart { get; }
+    private byte[] Memory { get; set; }
+
+    // Registers
+    private ushort I { get; set;}
+    private byte[] V { get; set; }
+    private byte DelayTimer { get; set; }
+    private byte SoundTimer { get; set; }
+
+    // Pseudo-registers
+    private byte StackPointer { get; set; }
+    private ushort ProgramCounter { get; set; }
+
+    private ushort[] Stack { get; set; } = new ushort[16];
+
+    public Chip8()
     {
-        Array.Copy(DefaultSprites.fontSet, Memory, 0x0);
+        RomStart = 0x200;
+        Memory = new byte[4096];
+
+        I = 0;
+        V = new byte[16];
+        DelayTimer = 0;
+        SoundTimer = 0;
+
+        StackPointer = 0;
+        ProgramCounter = RomStart;
     }
-    // Memory Map:
-    // +---------------+= 0xFFF (4095) End of Chip-8 RAM
-    // |               |
-    // |               |
-    // |               |
-    // |               |
-    // |               |
-    // | 0x200 to 0xFFF|
-    // |     Chip-8    |
-    // | Program / Data|
-    // |     Space     |
-    // |               |
-    // |               |
-    // |               |
-    // |               |
-    // |               |
-    // |               |
-    // +---------------+= 0x200 (512) Start of most Chip-8 programs
-    // | 0x000 to 0x1FF|
-    // | Reserved for  |
-    // |  interpreter  |
-    // +---------------+= 0x000 (0) Start of Chip-8 RAM
+
+    public void Run(byte[] rom)
+    {
+        //Setup Fonts
+        Array.Copy(DefaultSprites.fontSet, Memory, 0x0);
+
+        // Setup ROM
+        Array.Copy(rom, Memory, RomStart);
+    }
 }
