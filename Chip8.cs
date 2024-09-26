d8c0f23a60410fae8aa4ad20cd4153289a8fa66c
Dante <guchoo923@gmail.com>
2024-09-26 13:42:30 +0300

    add default fonts, add basic memory fields(will split up code further)

---
+++ Chip8.cs
@@ -0,0 +1,33 @@
+namespace Sharp8;
+
+public class Chip8
+{
+    private readonly ushort RomStart = 0x200; 
+    private byte[] Memory = new byte[4096];
+    private void SetupFonts()
+    {
+        Array.Copy(DefaultSprites.fontSet, Memory, 0x0);
+    }
+    // Memory Map:
+    // +---------------+= 0xFFF (4095) End of Chip-8 RAM
+    // |               |
+    // |               |
+    // |               |
+    // |               |
+    // |               |
+    // | 0x200 to 0xFFF|
+    // |     Chip-8    |
+    // | Program / Data|
+    // |     Space     |
+    // |               |
+    // |               |
+    // |               |
+    // |               |
+    // |               |
+    // |               |
+    // +---------------+= 0x200 (512) Start of most Chip-8 programs
+    // | 0x000 to 0x1FF|
+    // | Reserved for  |
+    // |  interpreter  |
+    // +---------------+= 0x000 (0) Start of Chip-8 RAM
+}
